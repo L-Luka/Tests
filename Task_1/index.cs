ffe3ad9b3c9411887b051771d3b5a5e315f1628d
L-Luka <138584383+L-Luka@users.noreply.github.com>
2024-03-10 19:44:42 +0100

    Add files via upload

---
+++ Task_1/index.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace EmployeeTimeTrackingApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public async Task<ActionResult> Index()
+        {
+            List<Employee> employees = await GetEmployees();
+            return View(employees);
+        }
+
+        private async Task<List<Employee>> GetEmployees()
+        {
+            List<Employee> employees = new List<Employee>();
+            string apiUrl = "https://rc-vault-fap-live-1.azurewebsites.net/api/gettimeentries?code={key}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+                if (response.IsSuccessStatusCode)
+                {
+                    string json = await response.Content.ReadAsStringAsync();
+                    employees = JsonConvert.DeserializeObject<List<Employee>>(json);
+                }
+                else
+                {
+                    // Handle error response
+                    // For simplicity, let's assume the error is logged
+                    Console.WriteLine("Error retrieving employee data: " + response.StatusCode);
+                }
+            }
+
+            return employees;
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int TotalTimeWorked { get; set; }
+    }
+}
